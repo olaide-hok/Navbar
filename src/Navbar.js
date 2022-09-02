0218dc362221361ae0b745fdd1ba7a0a12e1bcd3
olaide-hok <olaidekareem43@yahoo.com>
2022-09-02 12:16:01 +0100

    Complete Navbar Project.
    Setup useRef hook to dynamically render linksCtn using links div height.

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,6 +5,17 @@ import logo from './logo.svg'
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false)
+  const linksContainerRef = useRef(null)
+  const linksRef = useRef(null)
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`
+    } else {
+      linksContainerRef.current.style.height = '0px'
+    }
+  }, [showLinks])
   return (
     <nav>
       <div className="nav-center">
@@ -16,8 +27,8 @@ const Navbar = () => {
             <FaBars/>
           </button>
         </div>
-        <div className={`links-container ${showLinks && 'show-container'}`}>
-          <ul className="links">
+        <div className='links-container' ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
             {links.map((link) => {
               const {id, url, text} = link
               return (
